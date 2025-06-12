d377c845371dc533408b2f41c936ef91b5affd6c
Nicolas Draperi <nicolas.draperi2@gmail.com>
2025-06-12 13:11:34 +0200

    Ajout pagination

--- Crawler.py
+++ Crawler.py
@@ -17,7 +17,7 @@ ville = input("entrez la ville (ex: Paris) : ")
 adresse_complete = rue + " " + cp + " " + ville
 '''
 preference = input("Souhaitez-vous uniquement les consultations en visio, en présentiel, ou les deux ? (visio/presentiel/tout) : ").lower().strip() or "tout"
-assurance_pref = input("Souhaitez-vous un médecin conventionné d’un type précis ? (ex: secteur 1, secteur 2, non, tous) : ").lower().strip() or "tout"
+assurance_pref = input("Souhaitez-vous un médecin conventionné d’un type précis ? (ex: secteur 1, secteur 2, non, tous) : ").lower().strip() or "tous"
 
 specialite = "medecin"
 adresse_complete = "75000"
@@ -65,74 +65,81 @@ if specialite == "":
     search_btn.click()
     print("part c")
 
-time.sleep(2)
-cards = driver.find_elements(By.CSS_SELECTOR, "div.dl-card[data-design-system-component='Card']")
-print("selector card found")
 
 time.sleep(5)
 medecins = []
-
+base_url = driver.current_url.split("&page=")[0]
+page = 1
+max_pages = 4
 print('its fetching time')
 # cauchemardesque
-for card in cards:
-    try:
-        # Récup nom
-        nom_elem = card.find_element(By.CSS_SELECTOR, "h2")
-        nom = nom_elem.text.strip()
+while page <= max_pages:
+    current_url = base_url + f"&page={page}"
+    driver.get(current_url)
+    time.sleep(2)
+    cards = driver.find_elements(By.CSS_SELECTOR, "div.dl-card[data-design-system-component='Card']")
+    print("selector card found")
 
-        # Récup spécialité
-        spe = None
-        for div in card.find_elements(By.CSS_SELECTOR, "div.flex"):
-            if div.get_attribute("class").strip() == "flex":
-                spe = div.text.strip()
-                break
-        #recup adress
-        adresse = "Adresse non trouvée"
+    for card in cards:
         try:
-            bloc_adresse = card.find_element(By.CSS_SELECTOR, "div.flex.gap-8 div.flex.flex-wrap.gap-x-4")
-            lignes = bloc_adresse.find_elements(By.TAG_NAME, "p")
-            adresse = ", ".join(p.text.strip() for p in lignes if p.text.strip())
-        except:
-            pass
+            # Récup nom
+            nom_elem = card.find_element(By.CSS_SELECTOR, "h2")
+            nom = nom_elem.text.strip()
 
-        # Récup conventionnement
-        convention = "Conventionnement non trouvé"
-        blocs = card.find_elements(By.CSS_SELECTOR, "div.flex.gap-8")
-        for bloc in blocs:
-            try:
-                icone = bloc.find_element(By.CSS_SELECTOR, "svg")
-                aria_label = icone.get_attribute("aria-label").lower().strip()
-
-                if aria_label == "assurance":
-                    paragraphe = bloc.find_element(By.TAG_NAME, "p")
-                    convention = paragraphe.text.strip()
+            # Récup spécialité
+            spe = None
+            for div in card.find_elements(By.CSS_SELECTOR, "div.flex"):
+                if div.get_attribute("class").strip() == "flex":
+                    spe = div.text.strip()
                     break
+            #recup adress
+            adresse = "Adresse non trouvée"
+            try:
+                bloc_adresse = card.find_element(By.CSS_SELECTOR, "div.flex.gap-8 div.flex.flex-wrap.gap-x-4")
+                lignes = bloc_adresse.find_elements(By.TAG_NAME, "p")
+                adresse = ", ".join(p.text.strip() for p in lignes if p.text.strip())
             except:
+                pass
+
+            # Récup conventionnement
+            convention = "Conventionnement non trouvé"
+            blocs = card.find_elements(By.CSS_SELECTOR, "div.flex.gap-8")
+            for bloc in blocs:
+                try:
+                    icone = bloc.find_element(By.CSS_SELECTOR, "svg")
+                    aria_label = icone.get_attribute("aria-label").lower().strip()
+
+                    if aria_label == "assurance":
+                        paragraphe = bloc.find_element(By.TAG_NAME, "p")
+                        convention = paragraphe.text.strip()
+                        break
+                except:
+                    continue
+
+            # partie visio
+            visio = "Visio non disponible"
+            try:
+                video_icon = card.find_element(By.CSS_SELECTOR, "svg[aria-label='Consultation vidéo disponible']")
+                visio = "Visio disponible"
+            except:
+                visio = "Visio non disponible"
+
+            # filtre assurance
+            if assurance_pref != "tous" and assurance_pref not in convention.lower():
+                continue
+            # filtre visio
+            if (preference == "visio" and visio != "Visio disponible") or \
+                    (preference == "presentiel" and visio == "Visio disponible"):
                 continue
 
-        # partie visio
-        visio = "Visio non disponible"
-        try:
-            video_icon = card.find_element(By.CSS_SELECTOR, "svg[aria-label='Consultation vidéo disponible']")
-            visio = "Visio disponible"
-        except:
-            visio = "Visio non disponible"
+            if nom and spe:
+                medecins.append([nom, spe, adresse, convention,visio])
+            else:
+                print("info incomplète pour une card")
 
-        # filtre assurance
-        if assurance_pref != "tous" and assurance_pref not in convention.lower():
+        except Exception as e:
             continue
-        # filtre visio
-        if (preference == "visio" and visio != "Visio disponible") or \
-                (preference == "presentiel" and visio == "Visio disponible"):
-            continue
-
-        if nom and spe:
-            medecins.append([nom, spe, adresse, convention,visio])
-        else:
-            print("info incomplète pour une card")
-
-    except Exception as e:
-        continue
+    page += 1
 
 
 print("fetching complete")
